79e6198c20e64804d68469c45f284bd98c98d72c
Snow <17dcs013@lnmiit.ac.in>
2019-07-15 14:01:59 +0530

    depend-copy has dyanmic form

---
+++ static_dir/js/dependency.js
@@ -0,0 +1,16 @@
+// create new row
+$(document).on("click", ".btn-add-row", function(){
+    // clone row class element index 0
+    var row = $(".row").eq(0).clone().show();
+    console.log(row);
+    // append row clone to elemt-wraper
+    $(".element-wrapper").append(row);
+})
+
+// remove row handle
+$(document).on("click", ".btn-remove-row", function(){
+    // get btn index first
+    var index = $(".btn-remove-row").index(this);
+    // do remove row
+    $(".row").eq(index).remove();
+})
