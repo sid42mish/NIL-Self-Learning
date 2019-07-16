7127b5023e3eefd59a55e7063294ef9c30406366
tds-1 <tanmaydeepsharma21@gmail.com>
2019-07-16 13:54:07 +0530

    updated database

--- static_dir/js/preview.js
+++ static_dir/js/preview.js
@@ -1,21 +1,3 @@
-$('label').click(function() {
-    console.log(this)
-    if(this.attributes[0].value=="Fastlearning"){
-		const Url='/course/{{name}}/preview-course?id=0'
-  		$post(Url,fuction(){
-
-	    });
-    }	
-	else{
-		const Url='/course/{{name}}/preview-course?id=0'
-		$post(Url,fuction(){
-
-		});
-	}       
-})
-
-
-
 
 $(document).ready(fuction(){
 	$('#btnSubmit').click(function(){
